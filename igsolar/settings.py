ca17ad8a48372641f93768890c9ea14e85a2a732
Nikunj117 <nikunjpansuriya117@gmail.com>
2024-07-10 11:24:38 +0530

    changes in ss

--- igsolar/settings.py
+++ igsolar/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static_root')
+STATIC_URL = 'static_root/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
